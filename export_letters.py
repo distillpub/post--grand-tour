9aca1facc20e7f7ee70941894367942314f34246
jack <jack@email.com>
2020-01-01 23:33:03 -0700

    letters to the reviewers init

---
+++ export_letters.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sys
+
+df = pd.read_csv('todos.tsv', sep='\t')
+for reviewer in ['1', '2', '3']:
+    reviews = df[df.Reviewer == reviewer]
+
+    with open('letter-to-reviewer-{}.md'.format(reviewer), 'w') as f:
+        f.write('Letter to Reviewer #{}\n'.format(reviewer))
+        for quote, response, row in zip(reviews.Quote, reviews.Response, reviews.iterrows()):
+            try:
+                quote = quote[1:-1]#remove quotation marks
+                quote = quote.replace("'", '"')
+                for q in quote.split('\n'):
+                    f.write('> ' + q + '\n\n')
+                f.write('\n')
+                for r in response.split('\n'):
+                    f.write(r + '\n\n')
+                f.write('\n')
+            except:
+                print(row)
+                sys.exit()
